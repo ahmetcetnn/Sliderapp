b7c39a5fde7f86bdbd698943ae8424987cfe4be6
ahmetcetnn <ahmetlaw@outlook.com>
2023-01-26 16:21:47 +0300

    Sliderapp completed

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+   <h3>
+   Here are some photos of Crysis
+    </h3>   
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Components/SliderData.js
@@ -0,0 +1,24 @@
+export const SliderData = [
+    {
+        image:"https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2020/9/30/i4vg5oj5joqpxnxt87yt/crysis-remastered"
+    },
+    {
+        image:"https://www.nme.com/wp-content/uploads/2020/09/Crysis-Graphics.jpg"
+    },
+    {
+        image:"https://i.wpimg.pl/1280x/pg-uploads.wpcdn.pl/2020/04/crysis.jpg"
+    },
+    {
+        image:"https://multiplayer.net-cdn.it/thumbs/images/2012/05/16/crysis_3_screen_6_-_prophet_on_the_hunt_with_his_bow_jpg_1280x720_crop_q85.jpg"
+    },
+    {
+        image:"https://earlygame.com/uploads/images/_body/crysis-4-teaser-release-date.jpg"
+    },
+    {
+        image:"https://lvgames.info/wp-content/uploads//2022/01/34536-crysis-4.jpeg"
+    },
+    {
+        image:"https://images4.alphacoders.com/756/75666.jpg"
+    }
+
+]
